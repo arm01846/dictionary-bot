9c9ef77b1572c97f8423c30bf549dacbe09647fd
Sukrit Amornpornwiwat <sukrit@central.tech>
2018-11-05 13:07:30 +0700

    add methods for get meaning and synonym from Oxford

--- pkg/oxford.go
+++ pkg/oxford.go
@@ -1,5 +1,12 @@
 package bot
 
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+const OxfordBaseUrl = "https://od-api.oxforddictionaries.com/api/v1"
+
 type OxfordConfiguration struct {
 	AppID string
 	AppKey string
@@ -15,10 +22,44 @@ func NewOxfordClient(config *OxfordConfiguration) *OxfordClient {
 	}
 }
 
-func (oxford OxfordClient) Meaning(word string) string {
+func (oxford OxfordClient) Meaning(word string) (string, error) {
+	resp, err := http.Get(OxfordBaseUrl + "/entries/en/" + word)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	meaning := oxford.extractMeaning(body)
+
+	return meaning, nil
+}
+
+func (oxford OxfordClient) extractMeaning(json []byte) string {
 	return ""
 }
 
-func (oxford OxfordClient) Synonym(word string) string {
-	return ""
+func (oxford OxfordClient) Synonym(word string) ([]string, error) {
+	resp, err := http.Get(OxfordBaseUrl + "/entries/en/" + word + "/synonym")
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	synonym := oxford.extractSynonym(body)
+
+	return synonym, nil
+}
+
+func (oxford OxfordClient) extractSynonym(json []byte) []string {
+	return nil
 }
