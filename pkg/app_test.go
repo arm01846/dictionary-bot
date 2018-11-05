614a870eae393b6c2368a7b126dabad956ef7911
Sukrit Amornpornwiwat <sukrit@central.tech>
2018-11-05 12:45:05 +0700

    add more tests

--- pkg/app_test.go
+++ pkg/app_test.go
@@ -12,9 +12,15 @@ func Test_detectFirstWord(t *testing.T) {
 
 	t.Run("with single word should return that word back", func(t *testing.T) {
 		assert.Equal(t, "word", detectFirstWord("word"))
+		assert.Equal(t, "word", detectFirstWord(" word"))
 	})
 
 	t.Run("with more than one word should return the first word", func(t *testing.T) {
 		assert.Equal(t, "first", detectFirstWord("first second"))
+	})
+
+	t.Run("with non-english as first word should return empty string", func(t *testing.T) {
+		assert.Equal(t, "", detectFirstWord("1"))
+		assert.Equal(t, "", detectFirstWord("😂"))
 	})
 }
